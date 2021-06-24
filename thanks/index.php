cd0913cbe6177f7cd80ed81697bcdf1c55d39377
amdadulhaquemelon <amdadulhaquemelon63566@gmail.com>
2021-06-24 21:05:06 +0600

    jj

---
+++ thanks/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TXMMLDM');</script>
+<!-- End Google Tag Manager -->
+<link rel="stylesheet" href="../css/style.css">
+<title>火災保険・助成金　無料診断フォーム完了</title>
+</head>
+
+<!--<body style="display: relative;">-->
+<body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TXMMLDM"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+<!--<div class="p-container" style="max-width: 1200px; margin: 0 auto;position: absolute;  top: 50%;  left: 50%;  transform: translate(-50%, -50%);">-->
+  <div class="l-lp-wrap" style="padding: 40px 20px;">
+  <h2 style="margin-bottom: 20px; text-align: center;">火災保険・助成金　<br class="u-br__sp">無料診断フォーム完了</h2>
+  <p>この度はお問い合わせいただきまして、誠にありがとうございます。 <br>
+    <br>
+    入力いただいたアドレス宛に確認メールを送らせていただきました。見当たらない場合は、迷惑メールフォルダをご確認ください。<br>
+    <br>
+    なお、頂いた内容につきましては、弊社担当が内容を確認のうえ、3営業日以内に【048-225-6630】もしくは【090-3314-9215】からお電話させていただきます。<br>
+    <br>
+    何かご不明点等がございましたら、お気軽にお問い合わせください。 </p>
+  <div class="footer"> =======================<br>
+    富士貴建装 株式会社 <br>〒332-0006<br>
+    埼玉県川口市末広2丁目13番7号<br>
+    TEL：　<a href="tel:0800-919-1688">0800-919-1688</a>（フリーダイヤル）<br>
+    メール：　<a href="mailto:info@fujitaka-kensou.co.jp">info@fujitaka-kensou.co.jp</a><br>
+    HP：　<a href="https://fujitaka-kensou.jp" target="_blank">https://fujitaka-kensou.jp</a> </div>
+</div>
+</body>
+</html>
